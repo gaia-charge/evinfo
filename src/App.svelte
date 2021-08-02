59e39b8a08400bdca8e9b9a0a9baa9ce18d286ec
Wojtek Siudzinski <admin@suda.pl>
2021-08-02 08:49:48 +0200

    🌈 Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let name;
+	export let ready = false;
+
+	document.addEventListener("deviceready", () => {
+		ready = true;
+	})
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+{#if ready}
+	<h2>Cordova deviceready fired!</h2>
+{:else}
+	<h2>Waiting for cordova deviceready</h2>
+{/if}
